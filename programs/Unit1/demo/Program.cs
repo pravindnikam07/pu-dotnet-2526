314b4bff5bc8724fdea08959f3688e824364b229
pravindnikam07 <pravindnikam07@gmail.com>
2025-12-10 11:08:55 +0530

    dotnet console demo project setup

---
+++ programs/Unit1/demo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("Enter your name: ");
+    string name = Console.ReadLine();
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Welcome to C# and .NET development in VS Code!");
+    Console.WriteLine("Hello " + name);
+  }
+
+}
