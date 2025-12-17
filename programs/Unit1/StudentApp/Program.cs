cf362e2696b51a765f5c4eeccd713982b2dd4970
pravindnikam07 <pravindnikam07@gmail.com>
2025-12-17 11:14:10 +0530

    Student App

---
+++ programs/Unit1/StudentApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program {
+  static void Main(){
+    Console.WriteLine("Enter students details: ");
+    Console.WriteLine("Enter Student Name: ");
+    string name = Console.ReadLine();
+    Console.WriteLine("Enter Roll No: ");
+    int rollNo = int.Parse(Console.ReadLine());
+    Students student1 = new Students(name, rollNo);
+    student1.DisplayDetails();
+  }
+}
+
+class Students{
+  public string Name {get; set;}
+  public int RollNo {get; set;}
+
+  public Students() {
+
+  }
+
+  public Students(string name, int rollNo) {
+    Name = name;
+    RollNo = rollNo;
+  }
+
+  public void DisplayDetails() {
+    Console.WriteLine($"Name: {Name}, Roll No: {RollNo}");
+  }
+}
