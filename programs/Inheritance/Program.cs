a933ba640ea9bfd4c1804fe3d8239453a5d1e18b
pravindnikam07 <pravindnikam07@gmail.com>
2026-01-01 10:43:37 +0530

    Inheritance

---
+++ programs/Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Program
+{
+  static void Main(string[] args)
+  {
+    Dog dog1 = new Dog();
+    dog1.typeOfAnimal = "Dog";
+    dog1.name = "Buddy";
+    dog1.Speak();
+    dog1.Bark();
+
+  }
+}
+
+class Animal
+{
+  public String typeOfAnimal;
+  public virtual void Speak()
+  {
+    Console.WriteLine("Animal is speaking from Animal class... " + this.typeOfAnimal);
+  }
+}
+
+class Dog : Animal
+{
+  public string name;
+  public void Bark()
+  {
+    Console.WriteLine("Dog is barking from Dog class..." + this.name);
+  }
+
+  public override void Speak()
+  {
+    Console.WriteLine("Dog is speaking from Dog class... " + base.typeOfAnimal);
+  }
+
+}
+
+
