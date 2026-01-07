34040c702b7a8fdfe8d9b34e256a0533da0e10a0
pravindnikam07 <pravindnikam07@gmail.com>
2026-01-07 18:55:07 +0530

    db connection dotnet

---
+++ programs/Unit_3/dbConnection/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Microsoft.Data.Sqlite;
+namespace dbConnetion
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string connString = @"Data Source=dbtest.db";
+
+      using (SqliteConnection conn = new SqliteConnection(connString))
+      {
+        try
+        {
+          conn.Open();
+          Console.WriteLine("Opened database successfully");
+        }
+        catch (SqliteException ex)
+        {
+          Console.WriteLine($"[ERROR] {ex.Message}");
+        }
+      }
+    }
+  }
+}
